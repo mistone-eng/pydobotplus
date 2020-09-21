c684e7e68a7e5a2eaf26a7a2e714cffbc5b59c0c
ZdenekM <imaterna@fit.vutbr.cz>
2020-09-21 15:32:52 +0200

    Preparation for a release.

--- setup.py
+++ setup.py
@@ -4,17 +4,16 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='pydobot',
+    name='pydobot2',
     packages=['pydobot'],
-    version='1.0.2',
+    version='0.1.0',
     description='Python library for Dobot Magician',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    author='Luis Mesas',
-    author_email='luismesas@gmail.com',
-    url='https://github.com/luismesas/pydobot',
-    download_url='https://github.com/luismesas/pydobot/archive/v1.0.2.tar.gz',
-    keywords=['dobot', 'magician', 'robotics'],
+    author='Zdenek Materna',
+    author_email='imaterna@fit.vut.cz',
+    url='https://github.com/zdenekm/pydobot',
+    keywords=['dobot', 'magician', 'robotics', 'm1'],
     classifiers=[],
     install_requires=[
         'pyserial==3.4'
