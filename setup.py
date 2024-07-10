434b8a5fc09220cff46a4e567f74d9b8a5a2d154
sammydick22 <sjedickson@gmail.com>
2024-07-09 19:33:14 -0700

    setup2

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='pydobotplus',
-    packages=['pydobot'],
+    packages=['pydobotplus'],
     version='0.1.0',
     description='Python library for Dobot Magician upgraded',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    author='Zdenek Materna',
+    author='sammydick22',
     author_email='imaterna@fit.vut.cz',
-    url='https://github.com/zdenekm/pydobot',
+    url='https://github.com/sammydick22/pydobotplus',
     keywords=['dobot', 'magician', 'robotics', 'm1'],
     classifiers=[],
     install_requires=[
